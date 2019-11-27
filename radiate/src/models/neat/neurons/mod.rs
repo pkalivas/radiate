444fc8f6360019d5e77125cd2df68d2979bf5c38
Peter Kalivas <pkalivas@performancetrust.com>
2019-11-27 11:31:10 -0600

    clean up

--- radiate/src/models/neat/neurons/mod.rs
+++ radiate/src/models/neat/neurons/mod.rs
@@ -5,98 +5,94 @@ pub mod dense;
 
 
 pub mod activation {
-    
-use std::f64::consts::E as Eul;
+        
+    use std::f64::consts::E as Eul;
+
+    /// Varius activation functions for a neuron, must be specified at creation
+    #[derive(Debug, PartialEq, Clone, Copy)]
+    pub enum Activation {
+        Sigmoid,
+        Tahn,
+        Relu,
+        LeakyRelu(f64),
+        ExpRelu(f64),
+        Linear(f64)   
+    }
 
 
-/// Varius activation functions for a neuron, must be specified at creation
-#[derive(Debug, PartialEq, Clone, Copy)]
-pub enum Activation {
-    Sigmoid,
-    Tahn,
-    Relu,
-    LeakyRelu(f64),
-    ExpRelu(f64),
-    Linear(f64)   
-}
+    impl Activation {
 
-
-
-impl Activation {
-
-    /// Generic activation functions for an neural network - note for a few
-    /// of these an alpha parameter is needed when first assining the function
-    #[inline]
-    pub fn activate(&self, x: f64) -> f64 {
-        match self {
-            Self::Sigmoid => {
-                1.0 / (1.0 + (-x * 4.9).exp())
-            },
-            Self::Tahn => {
-                x.tanh()
-            },
-            Self::Relu => { 
-                if x > 0.0 { 
-                    x 
-                } else { 
-                    0.0 
+        /// Generic activation functions for an neural network - note for a few
+        /// of these an alpha parameter is needed when first assining the function
+        #[inline]
+        pub fn activate(&self, x: f64) -> f64 {
+            match self {
+                Self::Sigmoid => {
+                    1.0 / (1.0 + (-x * 4.9).exp())
+                },
+                Self::Tahn => {
+                    x.tanh()
+                },
+                Self::Relu => { 
+                    if x > 0.0 { 
+                        x 
+                    } else { 
+                        0.0 
+                    }
+                },
+                Self::Linear(alpha) => {
+                    alpha * x
+                },
+                Self::LeakyRelu(alpha) => {
+                    let a = alpha * x;
+                    if a > x {
+                        return a;
+                    } 
+                    x
+                },
+                Self::ExpRelu(alpha) => {
+                    if x >= 0.0 {
+                        return x;
+                    }
+                    alpha * (Eul.powf(x) - 1.0)
                 }
-            },
-            Self::Linear(alpha) => {
-                alpha * x
-            },
-            Self::LeakyRelu(alpha) => {
-                let a = alpha * x;
-                if a > x {
-                    return a;
-                } 
-                x
-            },
-            Self::ExpRelu(alpha) => {
-                if x >= 0.0 {
-                    return x;
-                }
-                alpha * (Eul.powf(x) - 1.0)
+            }
+        }
+
+
+
+        /// Deactivation functions for the activation neurons 
+        #[inline]
+        pub fn deactivate(&self, x: f64) -> f64 {
+            match self {
+                Self::Sigmoid => {
+                    x * (1.0 - x)
+                },
+                Self::Tahn => {
+                    1.0 - (x).powf(2.0)
+                },
+                Self::Linear(alpha) => {
+                    *alpha
+                },
+                Self::Relu => {
+                    if x > 0.0 { 
+                        return 1.0;
+                    }
+                    0.0
+                },
+                Self::ExpRelu(alpha) => {
+                    if x > 0.0 {
+                        return 1.0;
+                    }
+                    alpha * x.exp()
+                },
+                Self::LeakyRelu(alpha) => {
+                    if x > 0.0 { 
+                        return 1.0;
+                    } 
+                    *alpha 
+                },
             }
         }
     }
-
-
-
-    /// Deactivation functions for the activation neurons 
-    #[inline]
-    pub fn deactivate(&self, x: f64) -> f64 {
-        match self {
-            Self::Sigmoid => {
-                x * (1.0 - x)
-            },
-            Self::Tahn => {
-                1.0 - (x).powf(2.0)
-            },
-            Self::Linear(alpha) => {
-                *alpha
-            },
-            Self::Relu => {
-                if x > 0.0 { 
-                    return 1.0;
-                }
-                0.0
-            },
-            Self::ExpRelu(alpha) => {
-                if x > 0.0 {
-                    return 1.0;
-                }
-                alpha * x.exp()
-            },
-            Self::LeakyRelu(alpha) => {
-                if x > 0.0 { 
-                    return 1.0;
-                } 
-                *alpha 
-            },
-        }
-    }
-    
-
-}
 }
