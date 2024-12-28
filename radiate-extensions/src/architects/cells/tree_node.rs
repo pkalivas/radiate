d0912eedd7c8e249257ba0907b32bc0be166b3f3
Peter Kalivas <peterkalivas@gmail.com>
2024-12-28 07:08:23 -0600

    asdf

--- radiate-extensions/src/architects/cells/tree_node.rs
+++ radiate-extensions/src/architects/cells/tree_node.rs
@@ -1,4 +1,6 @@
-use super::NodeCell;
+use radiate::{Gene, Valid};
+
+use super::{expr::Expr, NodeCell};
 
 #[derive(PartialEq)]
 pub struct TreeNode<T> {
@@ -53,5 +55,49 @@ impl<T: Clone> Clone for TreeNode<T> {
                     .collect::<Vec<TreeNode<T>>>()
             }),
         }
+    }
+}
+
+impl<T> Gene for TreeNode<T>
+where
+    T: Clone + PartialEq + Default,
+{
+    type Allele = Expr<T>;
+
+    fn allele(&self) -> &Self::Allele {
+        &self.cell.value
+    }
+
+    fn new_instance(&self) -> Self {
+        TreeNode {
+            cell: self.cell.clone(),
+            children: self.children.as_ref().map(|children| {
+                children
+                    .iter()
+                    .map(|child| child.clone())
+                    .collect::<Vec<TreeNode<T>>>()
+            }),
+        }
+    }
+
+    fn with_allele(&self, allele: &Self::Allele) -> Self {
+        TreeNode {
+            cell: NodeCell {
+                value: allele.clone(),
+                ..self.cell.clone()
+            },
+            children: self.children.as_ref().map(|children| {
+                children
+                    .iter()
+                    .map(|child| child.clone())
+                    .collect::<Vec<TreeNode<T>>>()
+            }),
+        }
+    }
+}
+
+impl<T> Valid for TreeNode<T> {
+    fn is_valid(&self) -> bool {
+        todo!()
     }
 }
--- radiate-extensions/src/architects/node_collections/mod.rs
+++ radiate-extensions/src/architects/node_collections/mod.rs
@@ -1,17 +1,17 @@
+pub mod chromosome;
 pub mod codex;
 pub mod graph;
 pub mod iterators;
 pub mod node;
-pub mod node_chromosome;
 pub mod node_factory;
 pub mod reducers;
 pub mod tree;
 
+pub use chromosome::*;
 pub use codex::*;
 pub use graph::*;
 pub use iterators::*;
 pub use node::*;
-pub use node_chromosome::*;
 pub use node_factory::*;
 pub use reducers::*;
 pub use tree::*;
