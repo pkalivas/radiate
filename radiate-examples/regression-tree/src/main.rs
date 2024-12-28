e6ac31e4c4f50a7a8bb2bb7e1b2e14b896db8820
Peter Kalivas <peterkalivas@gmail.com>
2024-12-27 18:18:46 -0600

    tree crossover improvement

--- radiate-examples/regression-tree/src/main.rs
+++ radiate-examples/regression-tree/src/main.rs
@@ -6,7 +6,7 @@ const MIN_SCORE: f32 = 0.01;
 const MAX_SECONDS: f64 = 5.0;
 
 fn main() {
-    set_seed(42);
+    set_seed(200);
     let graph_codex = TreeCodex::regression(1, 3).set_gates(vec![op::add(), op::sub(), op::mul()]);
 
     let regression = Regression::new(get_sample_set(), ErrorFunction::MSE);
@@ -16,7 +16,7 @@ fn main() {
         .num_threads(10)
         .alter(alters!(
             TreeCrossover::new(0.5, 10),
-            NodeMutator::new(0.01, 0.05),
+            NodeMutator::new(0.1, 0.05),
         ))
         .fitness_fn(move |genotype: Tree<f32>| {
             let mut reducer = TreeReducer::new(&genotype);
