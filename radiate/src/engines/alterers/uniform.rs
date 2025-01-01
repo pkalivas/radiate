1fcf0309595e4b9fac013c3f117ca96eedb669d2
Peter Kalivas <peterkalivas@gmail.com>
2024-12-31 18:42:25 -0600

    some macro work

--- radiate/src/engines/alterers/uniform.rs
+++ radiate/src/engines/alterers/uniform.rs
@@ -2,6 +2,12 @@ use crate::alter::AlterType;
 use crate::Alter;
 use crate::Chromosome;
 
+use super::AltererTwo;
+use super::Crossover;
+use super::EngineComponent;
+use super::Mutator;
+
+#[derive(Clone)]
 pub struct UniformCrossover {
     pub rate: f32,
 }
@@ -25,6 +31,7 @@ impl<C: Chromosome> Alter<C> for UniformCrossover {
     }
 }
 
+#[derive(Clone)]
 pub struct UniformMutator {
     pub rate: f32,
 }
@@ -46,5 +53,41 @@ impl<C: Chromosome> Alter<C> for UniformMutator {
 
     fn alter_type(&self) -> AlterType {
         AlterType::Mutator
+    }
+}
+
+impl EngineComponent for UniformMutator {
+    fn name(&self) -> &'static str {
+        "UniformMutator"
+    }
+}
+
+impl AltererTwo for UniformMutator {
+    fn rate(&self) -> f32 {
+        self.rate
+    }
+
+    fn alter_type(&self) -> AlterType {
+        AlterType::Crossover
+    }
+}
+
+impl<C: Chromosome> Mutator<C> for UniformMutator {}
+
+impl<C: Chromosome> Crossover<C> for UniformCrossover {}
+
+impl EngineComponent for UniformCrossover {
+    fn name(&self) -> &'static str {
+        "UniformCrossover"
+    }
+}
+
+impl AltererTwo for UniformCrossover {
+    fn rate(&self) -> f32 {
+        self.rate
+    }
+
+    fn alter_type(&self) -> AlterType {
+        AlterType::Crossover
     }
 }
