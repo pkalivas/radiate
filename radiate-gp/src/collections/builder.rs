66372ec69edca1f38d40c26bf8843e7958377266
Peter Kalivas <peterkalivas@gmail.com>
2025-01-19 18:32:33 -0600

    Comments

--- radiate-gp/src/collections/builder.rs
+++ radiate-gp/src/collections/builder.rs
@@ -3,8 +3,8 @@
 /// This is a quality of life trait to help with the builder pattern or
 /// with things that can be 'built'. There are a multitude of things in
 /// the `radiate_gp` crate that rely on being 'built', and this trait is
-/// meant to abstract that out. For example, the `Tree`, `Graph', `TreeChromosome`,
-/// `GraphChromosome` can all be 'built' and thus implement this trait.
+/// meant to abstract that out. For example, the `Tree` and `Graph',
+/// can all be 'built' and thus implement this trait.
 ///
 /// # Example building a `Tree<Op<f32>>`:
 /// ```rust
@@ -22,6 +22,18 @@
 /// assert!(tree.height() == 3);
 /// assert!(tree.size() == 15);
 /// ```
+///
+/// The above will result in a tree that looks something like:
+/// ```text
+///       +
+///     /   \
+///    +     *
+///   / \   / \
+///  1   2 1   2
+/// ```
+/// **Note**: The above is not guaranteed, but is a good example of what
+/// the tree will look like. It isn't guaranteed because the `TreeBuilder`
+/// uses a random number generator to pick the value for each node.
 pub trait Builder {
     type Output;
     fn build(&self) -> Self::Output;
