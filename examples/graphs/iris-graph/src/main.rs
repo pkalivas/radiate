4149a6344866726f537004910d584d72421cc501
Peter Kalivas <peterkalivas@gmail.com>
2025-01-27 18:00:36 -0600

    e

--- examples/graphs/iris-graph/src/main.rs
+++ examples/graphs/iris-graph/src/main.rs
@@ -17,18 +17,18 @@ fn main() {
     let edges = vec![Op::weight(), Op::identity()];
     let outputs = vec![Op::sigmoid()];
 
-    let store = ops
-        .iter()
-        .chain(edges.iter())
-        .chain(outputs.iter())
-        .cloned()
-        .collect::<Vec<_>>();
+    // let store = ops
+    //     .iter()
+    //     .chain(edges.iter())
+    //     .chain(outputs.iter())
+    //     .cloned()
+    //     .collect::<Vec<_>>();
 
-    // let store = vec![
-    // (NodeType::Edge, edges.clone()),
-    // (NodeType::Vertex, ops.clone()),
-    // (NodeType::Output, outputs.clone()),
-    // ];
+    let store = vec![
+        (NodeType::Edge, edges.clone()),
+        (NodeType::Vertex, ops.clone()),
+        (NodeType::Output, outputs.clone()),
+    ];
 
     let codex = GraphCodex::asyclic(4, 4, store);
 
