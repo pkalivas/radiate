6538cf405e4aeb863fa26e6b2ceef4d4f12d3e3b
Peter Kalivas <peterkalivas@gmail.com>
2025-09-08 18:50:07 -0500

    bumping

--- crates/radiate-core/src/selector.rs
+++ crates/radiate-core/src/selector.rs
@@ -5,7 +5,7 @@ use crate::objectives::Objective;
 /// A trait for selection algorithms. Selection algorithms are used to select
 /// individuals from a [Population] to be used in the next generation. The
 /// selection process is (most of the time) based on the fitness of the individuals in the
-/// [Population]. The selection process can be based on the fitness of the individuals
+/// [Population]. The selection process can be based on the fitness of  the individuals
 /// in the [Population], or it can be based on the individuals themselves.
 pub trait Select<C: Chromosome>: Send + Sync {
     fn name(&self) -> &'static str {
