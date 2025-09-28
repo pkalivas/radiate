8829fbdeebf285ce6ef2c9666a9415c2337b6ca5
Peter Kalivas <peterkalivas@gmail.com>
2025-09-27 22:52:20 -0500

    a

--- py-radiate/src/lib.rs
+++ py-radiate/src/lib.rs
@@ -7,7 +7,9 @@ use radiate_python::{
     py_alter, py_select,
 };
 
-#[pymodule]
+// let gil_used = ;
+
+#[pymodule(gil_used = false)]
 fn radiate(m: &Bound<'_, PyModule>) -> PyResult<()> {
     radiate_python::init_logging();
 
