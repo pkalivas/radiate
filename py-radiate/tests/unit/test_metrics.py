ae5930866c6e9adf2be31340e337bd220f79a70b
Peter Kalivas <peterkalivas@gmail.com>
2025-11-11 19:16:44 -0600

    adding tests

--- py-radiate/tests/unit/test_metrics.py
+++ py-radiate/tests/unit/test_metrics.py
@@ -48,3 +48,59 @@ class TestMetrics:
                 assert metrics[key].min() is not None
                 assert metrics[key].max() is not None
                 assert metrics[key].count() is not None
+
+    @pytest.mark.integration
+    def test_metrics_from_events(self, random_seed):
+        class ScoreDistributionPlotter(rd.EventHandler):
+            """
+            Subscriber class to handle events and track metrics.
+            We will use this to plot score distributions over generations then
+            display the plot when the engine stops.
+            """
+
+            def __init__(self):
+                super().__init__(rd.EventType.EPOCH_COMPLETE)
+
+            def on_event(self, event: rd.EngineEvent) -> None:
+                metrics = event.metrics()
+                for key in metrics.keys():
+                    assert key in metrics
+                    if key == "scores":
+                        assert metrics[key].seq_mean() is not None
+                        assert metrics[key].seq_stddev() is not None
+                        assert metrics[key].seq_variance() is not None
+                        assert metrics[key].seq_kurt() is not None
+                        assert metrics[key].seq_skew() is not None
+                        assert metrics[key].seq_min() is not None
+                        assert metrics[key].seq_max() is not None
+                        assert (
+                            metrics[key].seq_count() == 100
+                        )  # num phenotypes in population
+                        assert len(metrics[key].seq_last()) == metrics[key].seq_count()
+                    elif key == "time" or "step" in key:
+                        assert metrics[key].time_last() is not None
+                        assert metrics[key].time_sum() is not None
+                        assert metrics[key].time_mean() is not None
+                        assert metrics[key].time_stddev() is not None
+                        assert metrics[key].time_variance() is not None
+                        assert metrics[key].time_min() is not None
+                        assert metrics[key].time_max() is not None
+                    elif "step" not in key:
+                        assert metrics[key].value_last() is not None
+                        assert metrics[key].mean() is not None
+                        assert metrics[key].stddev() is not None
+                        assert metrics[key].variance() is not None
+                        assert metrics[key].skew() is not None
+                        assert metrics[key].min() is not None
+                        assert metrics[key].max() is not None
+                        assert metrics[key].count() is not None
+
+        num_genes = 5
+        engine = rd.GeneticEngine(
+            codec=rd.IntCodec.vector(num_genes, init_range=(0, 10)),
+            fitness_func=lambda x: sum(x),
+            objective="min",
+            subscribe=[ScoreDistributionPlotter()]
+        )
+
+        engine.run([rd.ScoreLimit(0), rd.GenerationsLimit(500)])
